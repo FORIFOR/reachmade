579d11dfb5b74348023d17c2d5216065671bd02a
horio <145077644+FORIFOR@users.noreply.github.com>
2026-09-17 04:25:14 +0900

    test: include visual ownership responsive layer

--- tests/horio-premium.test.mjs
+++ tests/horio-premium.test.mjs
@@ -29,6 +29,7 @@ test('all design layers are imported before any CSS declarations',()=>{
     '@import url("./horio-premium.css");',
     '@import url("./precision-polish.css");',
     '@import url("./reachmade-art-direction.css");',
+    '@import url("./visual-ownership-responsive.css");',
   ]);
 });
 
